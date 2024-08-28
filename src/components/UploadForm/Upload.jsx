62a8c5493e4011acf70027e4d509316b8b9fe492
Abhishek kumar <sahaabhishek443@gmail.com>
2024-08-28 20:57:47 +0530

    clear message

--- src/components/UploadForm/Upload.jsx
+++ src/components/UploadForm/Upload.jsx
@@ -125,7 +125,7 @@ const StudentForm = () => {
                     <input
                       type="text"
                       id="subjects"
-                      name="subjects" // Removed unnecessary attribute for clarity
+                      name="subjects"
                       className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors   
  duration-200 ease-in-out"
                       onChange={handleSubjectChange}
